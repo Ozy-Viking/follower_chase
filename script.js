dc5c3bfb0ae6e3fca73c8c5d19b66b35a854616a
Ozy-Viking <admin@hankin.io>
2023-11-17 17:10:36 +1100

    initial commit

---
+++ script.js
@@ -0,0 +1,94 @@
+///////////////////////////
+// STREAMELEMENTS FIELDS //
+///////////////////////////
+
+let followerGoalTitle = "";
+let followerGoalDifference = 1;
+let googleFont = "Open Sans";
+let customFont = "";
+
+/////////////////
+// GLOBAL VARS //
+/////////////////
+
+let currentFollowerCount = 0;
+
+
+////////////////////
+// MAIN FUNCTIONS //
+////////////////////
+
+function LoadWidget() {
+    // Load the font
+    LoadFont();
+
+    // Update the follower goal label
+    let followerGoalTitleSpan = document.getElementById("followerGoalTitle");
+    followerGoalTitleSpan.innerHTML = followerGoalTitle;
+}
+
+function UpdateFollowerGoal() {
+    // Update the follower goal counts
+    let currentFollowerCountSpan = document.getElementById("currentFollowerCount");
+    currentFollowerCountSpan.innerHTML = currentFollowerCount;
+
+    let followerGoalCountSpan = document.getElementById("followerGoalCount");
+    followerGoalCountSpan.innerHTML = parseInt(currentFollowerCount) + parseInt(followerGoalDifference);
+}
+
+
+
+//////////////////////
+// HELPER FUNCTIONS //
+//////////////////////
+
+function LoadFont() {
+    document.body.style.fontFamily = IsNullOrWhitespace(customFont) ? googleFont : customFont;
+}
+
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours < 10) { hours = "0" + hours; }
+    //if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) { seconds = "0" + seconds; }
+    //return hours+':'+minutes+':'+seconds;
+    return minutes + ':' + seconds;
+}
+function IsNullOrWhitespace(str) {
+    return /^\s*$/.test(str);
+}
+
+
+
+///////////////////////////
+// STREAMELEMENTS EVENTS //
+///////////////////////////
+
+window.addEventListener('onWidgetLoad', function (event) {
+    console.log('onWidgetLoad', event);
+    const fieldData = event.detail.fieldData;
+    followerGoalTitle = fieldData.followerGoalTitle;
+    followerGoalDifference = fieldData.followerGoalDifference;
+    googleFont = fieldData.googleFont;
+    customFont = fieldData.customFont;
+
+    let data = event["detail"]["session"]["data"];
+    currentFollowerCount = data["follower-total"]["count"];
+    console.log(data)
+    LoadWidget();
+    UpdateFollowerGoal();
+});
+
+window.addEventListener('onSessionUpdate', function (event) {
+    console.log('onSessionUpdate', event.detail.session);
+    const data = event.detail.session;
+
+    currentFollowerCount = data["follower-total"]["count"];
+    UpdateFollowerGoal();
+
+    console.log(data);
+});
